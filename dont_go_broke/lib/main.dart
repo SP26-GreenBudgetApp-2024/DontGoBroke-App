ab292e0ea828897b3ed66f456655f5ebd0c030e9
kody-codes <kody.w.clark@gmail.com>
2024-10-08 13:55:12 -0400

    Changed Title Card to "Don't Go Broke"

--- dont_go_broke/lib/main.dart
+++ dont_go_broke/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'My Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Don\'t Go Broke '),
     );
   }
 }
