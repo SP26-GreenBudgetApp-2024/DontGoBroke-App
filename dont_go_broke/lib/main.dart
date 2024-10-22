0f2756a184bcff5b28d9ad5a2e74af19cb712fec
itsallme <itsallme@bellsouth.net>
2024-10-22 16:34:06 -0400

    Authentication, firebase, firestore initial back end json additions, additional configuration features and start of authentication classes.

--- dont_go_broke/lib/main.dart
+++ dont_go_broke/lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'screens/login_page.dart';
 
-void main() {
-  runApp(const MyApp());
+//integrations for Firebase 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+
+  // firebase communication entry 
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  // 
+
+runApp(const MyApp());
+
+  
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,3 +31,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
